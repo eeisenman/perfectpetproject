30a9a6e5ee11f2de87c81cfb146ccca183f1b1f0
phuongbao <phuonghtbao@gmail.com>
2017-08-01 13:59:25 -0700

    Update feed.js

--- feed.js
+++ feed.js
@@ -1,1 +1,75 @@
+var name = [
+  "Pepper", 
+  "Monte", 
+  "Pom Pom",
+  "Bubbles",
+  "Blossom",
+  "Buttercup",
+  "Lucky",
+  "Money", 
+  "Rhino", 
+  "Penny", 
+  "Fluffy",
+  "Bear", 
+  "Flash",
+  "Fawn",
+  "Bolt",
+  "Woody",
+  "Boo", 
+  "Duke", 
+  "Dolly",
+  "Milo" ]; 
+  
+  
 
+
+var age = [
+  "3 weeks",
+  "2 months",
+  "3 months",
+  "6 months",
+  "1 year old",
+  "2 years old",
+  "3 years old",
+  "4 years old",
+  "6 years old", 
+  "7 years old", 
+  "8 years old",
+  "9 years old",
+  "10 years old", 
+  "11 years old", 
+  "12 years old"];
+
+var breed = [
+  "unknown",
+  "mix",
+  "Corgi",
+  "Chihuahua", 
+  "Husky",
+  "Labrador", 
+  "Pug", 
+  "Labrador Retriever", 
+  "Beagle",
+  "German Shepard", 
+  "Shih Tzu",
+  "Bulldog", 
+  "Great Dane", 
+  "Pomeranian"]; 
+
+var gender = ["female", "male"]; 
+
+var neutered = ["neutered", "not neutered"] 
+
+var personality = [ 
+  "fun, energetic", 
+  "friendly, happy",
+  "calm, mellow", 
+  "outgoing, friendly", 
+  "loyal, confident", 
+  "shy, quiet"
+  "protective, alpha" 
+  "playful, kid friendly" ]; 
+
+
+
+ 
