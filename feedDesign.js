e5cefeb4aadf8ce2f8ee1fc4622da8d5c6666a79
phuongbao <phuonghtbao@gmail.com>
2017-08-03 15:44:16 -0700

    Update feedDesign.js

--- feedDesign.js
+++ feedDesign.js
@@ -4,7 +4,7 @@ var data = localStorage.getItem("testJSON");
 var text = "<ul>";
 
 for (i = 0; i < data.length; i++) { 
-    text += "<p>" + data[i] + "</h2>" + "</p>" 
+    text += "<p>" + data[i] + "</p>" 
 }
 
 text+= "</ul>";
