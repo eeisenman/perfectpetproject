2262aadcbcda2a47b23d4e7d8eb211f01620fd19
phuongbao <phuonghtbao@gmail.com>
2017-08-03 14:12:08 -0700

    Create feedDesign.js

---
+++ feedDesign.js
@@ -0,0 +1,7 @@
+for pet in data: 
+  var text = "<h1>" + petName + "</h1>" +
+             "<h2>" + petType + "</h2>"+ 
+             "<h3>" + petGender + "</h3>"+ 
+             "<h4>" + petNeuter + "</h4>"+ 
+             "<h5>" + petAge + "</h5>"+ 
+             "<h5>" + petAge + "</h5>"+ 
