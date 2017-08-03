cb7caa57c996b01f59c1ec981eb463519e79bc98
phuongbao <phuonghtbao@gmail.com>
2017-08-03 14:27:55 -0700

    Update feedDesign.js

--- feedDesign.js
+++ feedDesign.js
@@ -1,11 +1,11 @@
 for pet in data: 
   var text = "<h1>" + petName + "</h1>" +
-             "<h2>" + petType + "</h2>"+ 
-             "<h2>" + petGender + "</h2>"+ 
-             "<h2>" + petNeuter + "</h2>"+ 
-             "<h2>" + petAge + "</h2>"+ 
-             "<h2>" + petSize + "</h2>"+ 
-             "<h2>" + extraOptions + "</h2>"+ 
+             "<h1>" + petType + "</h1>"+ 
+             "<h1>" + petGender + "</h1>"+ 
+             "<h1>" + petNeuter + "</h1>"+ 
+             "<h1>" + petAge + "</h1>"+ 
+             "<h1>" + petSize + "</h1>"+ 
+             "<h1>" + extraOptions + "</h1>"+ 
              "<img>" + image + "</img>";
 
 
