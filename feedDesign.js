43836036030cb34b549e4b0c3f32a25509ea42c0
phuongbao <phuonghtbao@gmail.com>
2017-08-03 15:23:13 -0700

    Update feedDesign.js

--- feedDesign.js
+++ feedDesign.js
@@ -4,14 +4,7 @@ var data = localStorage.getItem("testJSON");
 var text = "<ul>";
 
 for (i = 0; i < data.length; i++) { 
-    text += "<h2>" + petName[i] + "</h2>" + "<br>" + 
-            "<p>" + petType[i] + "</p>" + "<br>" + 
-            "<p>" + petGender[i] + "</p>" + "<br>" +   
-            "<p>" + petNeuter[i] + "</p>" + "<br>" + 
-            "<p>" + petAge[i] + "</p>" + "<br>" + 
-            "<p>" + petSize[i] + "</p>" + "<br>" + 
-            "<p>" + extraOptions[i] + "</p>" + "<br>" + 
-            "<img>" + photo[i] + "</img>" + "<br>";
+    text += "<p>" + data[i] + "</h2>" + "</p>" 
 }
 
 text+= "</ul>";
