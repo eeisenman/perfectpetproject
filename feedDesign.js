4210f45d7d392f34637103355766fa3c574f147c
phuongbao <phuonghtbao@gmail.com>
2017-08-03 15:21:10 -0700

    Update feedDesign.js

--- feedDesign.js
+++ feedDesign.js
@@ -1,16 +1,20 @@
 var data = localStorage.getItem("testJSON"); 
 
 
+var text = "<ul>";
+
 for (i = 0; i < data.length; i++) { 
-    text += "<ul>" + "<h2>" + petName[i] + "</h2>" + "</ul>" + "<br>" + 
-            "<ul>" + "<p>" + petType[i] + "</p>" + "</ul>" + "<br>" + 
-            "<ul>" + "<p>" + petGender[i] + "</p>" + "</ul>" + "<br>" +   
-            "<ul>" + "<p>" + petNeuter[i] + "</p>" + "</ul>" + "<br>" + 
-            "<ul>" + "<p>" + petAge[i] + "</p>" + "</ul>" + "<br>" + 
-            "<ul>" + "<p>" + petSize[i] + "</p>" + "</ul>" + "<br>" + 
-            "<ul>" + "<p>" + extraOptions[i] + "</p>" + "</ul>" + "<br>" + 
-            "<ul>" + "<img>" + photo[i] + "</img>" + "</ul>" + "<br>";
+    text += "<h2>" + petName[i] + "</h2>" + "<br>" + 
+            "<p>" + petType[i] + "</p>" + "<br>" + 
+            "<p>" + petGender[i] + "</p>" + "<br>" +   
+            "<p>" + petNeuter[i] + "</p>" + "<br>" + 
+            "<p>" + petAge[i] + "</p>" + "<br>" + 
+            "<p>" + petSize[i] + "</p>" + "<br>" + 
+            "<p>" + extraOptions[i] + "</p>" + "<br>" + 
+            "<img>" + photo[i] + "</img>" + "<br>";
 }
+
+text+= "</ul>";
 
 
       
