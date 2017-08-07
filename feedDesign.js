8ad160d318a55fd0f66070955bc5565c13cd76e5
emoore01 <pinkalicious1101@gmail.com>
2017-08-07 13:33:55 -0700

    Update feedDesign.js

--- feedDesign.js
+++ feedDesign.js
@@ -8,7 +8,7 @@ var text = "<ul>";
 for (i = 0; i < obj.length; i++) { 
     var pet = obj[i];
     text += "<img src=" + pet["photo"] + ">";
-    text += "<center><li><b>" + "Name: " + pet["petName"] + "</b></li></center><br>";
+    text += "<li><b>" + "Name: " + pet["petName"] + "</b></li><br>";
     text += "<li>" + "Pet Gender: " + pet["petGender"] + "</li><br>";
     text += "<li>" + "Pet Type: " + pet["petType"] + "</li><br>";
     text += "<li>" + "Age: "+ pet["petAge"] + "</li><br>";
