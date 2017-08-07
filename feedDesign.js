dffa60825a66486c590ac6950f8c7b9fccd589ae
eeisenman <ericaeisenman1@gmail.com>
2017-08-07 00:06:10 -0700

    Update feedDesign.js

--- feedDesign.js
+++ feedDesign.js
@@ -7,7 +7,7 @@ var text = "<ul>";
 
 for (i = 0; i < obj.length; i++) { 
     var pet = obj[i];
-    text += "<div class="polaroid"><img src=" + pet["photo"] + ">";
+    text += "<img src=" + pet["photo"] + ">";
     text += "<li>" + pet["petName"] + "</li></div><br>";
     text += "<li>" + pet["petType"] + "</li></div><br>";
     text += "<li>" + pet["petAge"] + "</li></div><br>";
